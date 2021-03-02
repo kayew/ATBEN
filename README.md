951623a587461192e9730c56006ca0c2d0c66cb3
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2021-03-02 00:07:41 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# ATBEN
+
+Just say the magic word "ATBEN" (or "atben") and Ben is pinged. Problem solved.
+
+How to set up:
+1. Install [luvit](https://luvit.io/)
+2. Install [Discordia](https://github.com/SinisterRectus/Discordia) in this repo
+3. Put your client token in a file called `client_token`
+4. Run the bot with `luvit main.lua client_token`
+5. ???
+6. Profit!
+
---
+++ main.lua
@@ -0,0 +1,22 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+
+local function readToken()
+  local file = io.open(args[2], "r")
+  local token = file:read()
+  file:close()
+  return token
+end
+
+client:on('ready', function()
+	print('Ready to ATBEN, signed in as:'.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+  if message.content:find("atben") or message.content:find("ATBEN") then
+    message.channel:send('<@400073201700569098>')
+  end
+end)
+
+client:run('Bot ' .. readToken())
+
