be3c6936abb5304cbf6f46749d66fd35c1990f9f
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2021-03-02 10:09:32 -0500

    cleanup and commenting

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
+-- DO NOT DELETE THESE LINES
 local discordia = require('discordia')
 local client = discordia.Client()
 
 local function readToken()
+  -- Takes 2nd argument of command line (a file) and returns bot token
   local file = io.open(args[2], "r")
   local token = file:read()
   file:close()
@@ -9,10 +11,11 @@ local function readToken()
 end
 
 client:on('ready', function()
-  print('Ready to ATBEN, signed in as:'.. client.user.username)
+  print('Ready to ATBEN, signed in as: '.. client.user.username)
 end)
 
 client:on('messageCreate', function(message)
+  -- Converts message to all lowercase, looks for "atben". If true, ping Ben.
   if message.content:lower():find("atben") then
     message.channel:send('<@400073201700569098>')
   end
